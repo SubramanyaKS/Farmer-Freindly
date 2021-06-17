b08c32a7d9b7940c9be77d40188bcd2d2cc6fa25
Puneeth_Aacharya <68151137+Puneeth-Aacharya@users.noreply.github.com>
2021-06-17 15:43:44 +0530

    Add files via upload

--- farmer_signup.js
+++ farmer_signup.js
@@ -8,12 +8,13 @@ function newuser()
 		var pass=document.getElementById("password").value;
 		var mail=document.getElementById("email").value;
 		var cpass=document.getElementById("confirm_password").value;
+		
 
 
 		var txtphone=document.form.phone.value;
 		var phonetest=txtphone.match(/^[0-9]{10}$/);
 		var txtpincode=document.form.pincode.value;
-		var pintest=txtpincode.match(/^[1-9]{6}$/);
+		var pintest=txtpincode.match(/^[0-9]{6}$/);
 		var txtemail=document.form.email.value;
 		var emailtest=txtemail.match(/^[A-Za-z0-9]*@gmail.com$/);
 		var txtpassword=document.form.password.value;
@@ -25,29 +26,28 @@ function newuser()
 		{
 			alert("Please Enter Required information");
 		}
-
-		
-		else if(txtphone!=phonetest)
-		{
-			alert("Invalid mobile number");
-		}
-		if(cpass!=pass)
-		{
-			alert("Invalid password");
-			
-		}
-		else if(txtpincode!=pintest)	
-		{
-			alert("Invalid Pincode");
-				
-		}
-		/*else if(mail!=null)
+		/* if(mail!=null)
 		{
 			if(txtemail!=emailtest){
 				alert("Invalid email id");
 			}
 		}*/
-		else if(txtpassword!=passtest)
+		
+		 if(txtphone!=phonetest)
+		{
+			alert("Invalid mobile number");
+		}
+		 if(cpass!=pass)
+		{
+			alert("Invalid password");
+			
+		}
+		 if(txtpincode!=pintest)	
+		{
+			alert("Invalid Pincode");
+				
+		}
+		 if(txtpassword!=passtest)
 		{
 			alert("Password should contain one uppercase letter,special char.and number");
 			
@@ -55,6 +55,6 @@ function newuser()
 		else
 		{
 			alert("Registration succesful,  Please Login");
-			//window.location.href="login.html";
+			window.location.href="login.html";
 		}
 	}
