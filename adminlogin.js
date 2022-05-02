ec3ef52e75c8efbe6e218936c2dbef9ec110e2db
Subramanya K S <subramanyaks2205@gmail.com>
2022-05-02 06:46:58 +0530

    Update adminlogin.js

--- adminlogin.js
+++ adminlogin.js
@@ -1,15 +1,17 @@
 function validate()
 {
 	var username=document.getElementById("username").value;
-	varvpassword=document.getElementById("password").value;
+	var password=document.getElementById("password").value;
 	if(username=="admin"&& password=="user")
 	{
 		alert("login succesfully");
+		window.location.href="login.html";
 		return false;
 	}
 	else
 	{
 		alert("login failed");
+		window.location.href="Adminlogin.html";
 		
 	}
 }
